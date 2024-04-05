2d9c9cf34e671c5a192244d977654a7970655fcc
Taichi93258 <taiponpon0308@gmail.com>
2024-04-06 05:43:47 +0900

    メルカリの商品タイトルとカテゴリが対応づけられたcsvファイルから機械学習によってモデルを作り、フォーム送られてきたタイトルからカテゴリを予測する処理を作成

--- nlp/views.py
+++ nlp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import pandas as pd
+import pickle
 
-# create your views here
+category_data = pd.read_csv("idx2category.csv")
+idx2category = {row.k: row.v for idx, row in category_data.iterrows()}
+
+with open("rdmf.pickle", mode="rb") as f:
+    model = pickle.load(f)
+
+# Create your views here.
+
+def index(request):
+    if request.method ==  'GET':
+        return render(
+        request,
+        "nlp/home.html"
+    )
+    else:
+        title = [request.POST.get('title')]
+        result = model.predict(title)[0]
+        pred =  idx2category[result]
+        return render(request, 'nlp/home.html', {'title': pred})
